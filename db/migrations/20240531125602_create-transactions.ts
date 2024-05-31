2b1f67b2da827642908dffcbb60586bf7bcc6456
FellpsGN <fellps.program06@gmail.com>
2024-05-31 17:18:05 -0300

    Adicionado await na add-session-id-to-transactions e ponto e virgula para padronização

--- db/migrations/20240531125602_create-transactions.ts
+++ db/migrations/20240531125602_create-transactions.ts
@@ -7,11 +7,11 @@ export async function up(knex: Knex): Promise<void> {
         table.text("title").notNullable();
         table.decimal("amount", 10, 2).notNullable();
         table.timestamp("created_at").defaultTo(knex.fn.now()).notNullable();
-    })
-}
+    });
+};
 
 
 export async function down(knex: Knex): Promise<void> {
     await knex.schema.dropTable("transactions");
-}
+};
 
