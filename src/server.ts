13cd247feb1bfbc9eca2891e3e82bd863c5ac9b5
FellpsGN <fellps.program06@gmail.com>
2024-05-31 21:06:42 -0300

    Ajuste de ponto e vírgula

--- src/server.ts
+++ src/server.ts
@@ -7,7 +7,7 @@ import { transactionsRoutes } from "./routes/transactions";
 const app = fastify();
 
 
-app.register(cookie)
+app.register(cookie);
 app.register(transactionsRoutes, 
     {
         prefix: "transactions"
